f0363433ab153896d92691013b1d8bd8d8b3caf3
polaco <kalvarezholenderski@gmail.com>
2019-09-14 11:08:34 -0300

    clase 14 septiembre

---
+++ clase 14.R
@@ -0,0 +1,39 @@
+x <- 2
+print (x)
+x <- 3
+y <- 10
+x < y
+print (x)
+a <- x >= y
+print (x)
+b <- x > y
+print (x)
+c <- x <= y
+print (x)
+d <- x == y
+
+if (a==TRUE){ 
+  x <- x+6
+  }else{
+    x<- x-2
+    }
+print(x)
+
+X <- x<y | ((c==TRUE) & (a==FALSE))
+print (X)
+if (x<=y){
+  x<- (x*y)/3 - x%%y
+}else{
+  x<- x-(x-y)
+}else{
+  x<- x+1
+}
+
+z<- 1
+if(z>0){
+  print (2*z)
+  z<- 2*z
+}
+#double
+#si z>0 entonces 2*z
+#
